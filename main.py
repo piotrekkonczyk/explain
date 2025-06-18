81c9db6943629a573b9bb7e5a853b21be04a75b9
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-06-18 08:33:47 +0200

    feat: added typer command

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from ollama import chat
+import ollama
 import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def ask(question: str):
+    response = ollama.chat(
+        model="llama3", messages=[{"role": "user", "content": question}]
+    )
+
+    print(response.message.content)
 
 
 def main(message: str):
-    response = chat(model="llama3", messages=[{"role": "user", "content": message}])
+    response = ollama.chat(
+        model="llama3", messages=[{"role": "user", "content": message}]
+    )
 
     print(response.message)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
