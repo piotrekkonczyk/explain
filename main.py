f3dde61aadb645f74d1113ee50622564064bbffa
piotrekkonczyk <konczyk.piotrek@gmail.com>
2025-06-18 08:41:15 +0200

    feat: created a client and added small context message

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import typer
 
 app = typer.Typer()
 
+client = ollama.Client()
+model = "llama3"
+
 
 @app.command()
 def ask(question: str):
-    response = ollama.chat(
-        model="llama3", messages=[{"role": "user", "content": question}]
+    response = client.chat(
+        model="llama3",
+        messages=[
+            {
+                "role": "user",
+                "content": "Your goal will be to answer the given question",
+            },
+            {"role": "user", "content": question},
+        ],
     )
 
     print(response.message.content)
-
-
-def main(message: str):
-    response = ollama.chat(
-        model="llama3", messages=[{"role": "user", "content": message}]
-    )
-
-    print(response.message)
 
 
 if __name__ == "__main__":
